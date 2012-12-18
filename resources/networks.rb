a1e493fb3693b238a8e14003f12d9a74965f9ad5
Jason Cannavale <jason.cannavale@rackspace.com>
2012-12-18 09:34:06 -0600

    making num_networks, net_size either string or integer for compat with current deployments

--- resources/networks.rb
+++ resources/networks.rb
@@ -1,11 +1,27 @@
+# Cookbook Name:: nova-network
+# Resource:: networks
+#
+# Copyright 2012, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 actions :create_fixed, :create_floating
 
 # nova-manage network create cli options
 attribute :label, :kind_of => String
 attribute :multi_host, :kind_of => [ TrueClass, FalseClass], :default => true
 attribute :fixed_range, :kind_of => String
-attribute :num_networks, :kind_of => Integer
-attribute :net_size, :kind_of => Integer
+attribute :num_networks, :kind_of => [ String, Integer ]
+attribute :net_size, :kind_of => [ String, Integer ]
 attribute :bridge, :kind_of => String
 attribute :bridge_int, :kind_of => String
 attribute :dns1, :kind_of => String
