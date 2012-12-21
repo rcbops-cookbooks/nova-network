2fd90c5421dd9910e285bb7b61ec74ef7a0cd53d
Joseph W. Breu <breu@breu.org>
2012-12-21 12:18:17 -0600

    DEFECT: nova-network creates network with multi_host=0 when it should be 1

--- providers/networks.rb
+++ providers/networks.rb
@@ -16,9 +16,12 @@
 # limitations under the License.
 action :create_fixed do
     log "Creating network #{new_resource.label}"
-
+    multi_host_bool_mangle="F"
+    if new_resource.multi_host
+      multi_host_bool_mangle="T"
+    end
     execute "nova-manage network create --label=#{new_resource.label}" do
-	    command "nova-manage network create --multi_host=#{new_resource.multi_host} --label=#{new_resource.label} --fixed_range_v4=#{new_resource.fixed_range} --num_networks=#{new_resource.num_networks} --network_size=#{new_resource.net_size} --bridge=#{new_resource.bridge} --bridge_interface=#{new_resource.bridge_int} --dns1=#{new_resource.dns1} --dns2=#{new_resource.dns2}"
+	    command "nova-manage network create --multi_host=#{multi_host_bool_mangle} --label=#{new_resource.label} --fixed_range_v4=#{new_resource.fixed_range} --num_networks=#{new_resource.num_networks} --network_size=#{new_resource.net_size} --bridge=#{new_resource.bridge} --bridge_interface=#{new_resource.bridge_int} --dns1=#{new_resource.dns1} --dns2=#{new_resource.dns2}"
 	    action :run
 	    not_if "nova-manage network list | grep #{new_resource.fixed_range}"
     end
