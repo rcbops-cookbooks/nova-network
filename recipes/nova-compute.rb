ba012dcb14875941861dec720cf5da39965edf0f
Jason Cannavale <jason.cannavale@rackspace.com>
2013-01-03 09:53:44 -0600

    adding include_recipes to bring in necessary pieces based on nova[network][provider] flag. this is for role support

--- recipes/nova-compute.rb
+++ recipes/nova-compute.rb
@@ -1,8 +1,14 @@
 #
 # Cookbook Name:: nova-network
-# Recipe:: default
+# Recipe:: nova-compute
 #
 # Copyright 2012, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
+
+if node["nova"]["network"]["provider"] == "quantum"
+	include_recipe "nova-network::quantum-plugin"
+else
+	include_recipe "nova-network::nova-network"
+end
--- recipes/nova-controller.rb
+++ recipes/nova-controller.rb
@@ -1,8 +1,15 @@
 #
 # Cookbook Name:: nova-network
-# Recipe:: default
+# Recipe:: nova-controller
 #
 # Copyright 2012, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
+
+if node["nova"]["network"]["provider"] == "quantum"
+	include_recipe "nova-network::quantum-server"
+else
+	include_recipe "nova-network::nova-network"
+	include_recipe "nova-network::nova-setup"
+end
