3966c4b84d011d72f1d02dfb13749bf8b1483351
rcbjenkins <rcb-deploy@lists.rackspace.com>
2013-02-06 11:35:30 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace US, Inc."
 license           "Apache 2.0"
 description       "Installs and configures Openstack"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.17"
+version           "1.0.18"
 recipe		  "network", ""
 
 %w{ ubuntu fedora redhat centos }.each do |os|
