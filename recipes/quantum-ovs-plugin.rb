6d5298bf2661255899a7c4d4c931ee05e7ac76ac
Darren Birkett <darren.birkett@gmail.com>
2013-06-14 11:56:30 +0100

    refactor common configs into quantum-common recipe

--- recipes/quantum-ovs-plugin.rb
+++ recipes/quantum-ovs-plugin.rb
@@ -16,9 +16,11 @@
 # limitations under the License.
 
 include_recipe "osops-utils"
+include_recipe "nova-network::quantum-common"
 
 platform_options = node["quantum"]["platform"]
 plugin = node["quantum"]["plugin"]
+
 
 node["quantum"][plugin]["packages"].each do |pkg|
   package pkg do
@@ -30,68 +32,25 @@ end
 service "quantum-plugin-openvswitch-agent" do
   service_name node["quantum"]["ovs"]["service_name"]
   supports :status => true, :restart => true
-  action :nothing
+  action :enable
+  subscribes :restart, "template[/etc/quantum/quantum.conf]", :delayed
+  subscribes :restart, "template[/etc/quantum/ovs_quantum_plugin.ini]", :delayed
 end
 
 service "openvswitch-switch" do
   service_name "openvswitch-switch"
   supports :status => true, :restart => true
-  action :nothing
+  action :enable
+  subscribes :restart, "template[/etc/quantum/quantum.conf]", :delayed
+  subscribes :restart, "template[/etc/quantum/ovs_quantum_plugin.ini]", :delayed
 end
 
 mysql_info =
   get_access_endpoint("mysql-master", "mysql", "db")
-ks_admin_endpoint =
-  get_access_endpoint("keystone-api", "keystone", "admin-api")
-rabbit_info =
-  get_access_endpoint("rabbitmq-server", "rabbitmq", "queue")
-api_endpoint =
-  get_access_endpoint("nova-network-controller", "quantum", "api")
-### FIXME
-local_ip =
-  get_ip_for_net('nova', node)
 quantum_info =
   get_settings_by_recipe("nova-network\\:\\:nova-controller", "quantum")
-
-template "/etc/quantum/api-paste.ini" do
-  source "api-paste.ini.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables(
-    "keystone_api_ipaddress" => ks_admin_endpoint["host"],
-    "keystone_admin_port" => ks_admin_endpoint["port"],
-    "keystone_protocol" => ks_admin_endpoint["scheme"],
-    "service_tenant_name" => quantum_info["service_tenant_name"],
-    "service_user" => quantum_info["service_user"],
-    "service_pass" => quantum_info["service_pass"]
-  )
-end
-
-template "/etc/quantum/quantum.conf" do
-  source "quantum.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  variables(
-    "quantum_debug" => node["quantum"]["debug"],
-    "quantum_verbose" => node["quantum"]["verbose"],
-    "quantum_ipaddress" => api_endpoint["host"],
-    "quantum_port" => api_endpoint["port"],
-    "rabbit_ipaddress" => rabbit_info["host"],
-    "rabbit_port" => rabbit_info["port"],
-    "overlapping_ips" => node["quantum"]["overlap_ips"],
-    "quantum_plugin" => node["quantum"]["plugin"],
-    "quota_items" => node["quantum"]["quota_items"],
-    "default_quota" => node["quantum"]["default_quota"],
-    "quota_network" => node["quantum"]["quota_network"],
-    "quota_subnet" => node["quantum"]["quota_subnet"],
-    "quota_port" => node["quantum"]["quota_port"],
-    "quota_driver" => node["quantum"]["quota_driver"]
-  )
-  notifies :restart, "service[quantum-server]", :immediately
-  notifies :enable, "service[quantum-server]", :immediately
-end
+local_ip =
+  get_ip_for_net('nova', node)
 
 template "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
   source "ovs_quantum_plugin.ini.erb"
@@ -114,9 +73,6 @@ template "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
     "ovs_verbose" => node["quantum"]["verbose"],
     "ovs_local_ip" => local_ip
   )
-  notifies :restart, "service[quantum-plugin-openvswitch-agent]", :immediately
-  notifies :enable, "service[quantum-plugin-openvswitch-agent]", :immediately
-  notifies :restart, "service[openvswitch-switch]", :immediately
 end
 
 execute "create integration bridge" do
