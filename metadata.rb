88fa5bcc9f7166f83834874dc823ae56995d60cd
rackerjoe <breu@breu.org>
2013-09-09 16:17:21 -0500

    Updating version # to 4.1.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ name              "nova-network"
 license           "Apache 2.0"
 description       "Installs and configures the networking required for Openstack"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.21"
+version           "4.1.2"
 recipe            "nova-compute", ""
 recipe            "nova-controller", ""
 recipe            "nova-network", ""
